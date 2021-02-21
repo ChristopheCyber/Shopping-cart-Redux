732559e2801ec304730b97d5eddf9129c517f1ae
Christophe <christophalpha@yahoo.com>
2021-02-21 23:44:31 +0100

    install THUNK yarn add redux-thunk

--- src/indexOldFromCRA.js
+++ src/indexOldFromCRA.js
@@ -1,19 +1,8 @@
-//OLD from CRA:
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//NEW:
-/*import { render } from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import reducer from './reducers'
-import { getAllProducts } from './actions'*/
-// import App from './containers/App'
-
 
 ReactDOM.render(
   <React.StrictMode>
